f662360b975cf62bba1829e3fa92fb2a4b798a22
Clifford Mapesa <97587370+droffilc1@users.noreply.github.com>
2022-07-16 12:35:27 +0300

    Add files via upload

---
+++ src/Componenets/Overview.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Overview = (props) => {
+  const { tasks } = props;
+
+  return (
+    <ol>
+      {tasks.map((task) => {
+        return <li key={task.id}>{task.text}</li>;
+      })}
+    </ol>
+  );
+};
+
+export default Overview;
